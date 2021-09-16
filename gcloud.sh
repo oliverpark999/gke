67df753a6323c04323845fac2644aa1eb7dd65eb
oliverpark999 <seoul.milk@gmail.com>
2021-09-16 11:26:27 +0900

    210916_GKE 세팅

---
+++ gcloud.sh
@@ -0,0 +1,6 @@
+ curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-308.0.0-linux-x86_64.tar.gz
+ tar xvzf google-cloud-sdk-308.0.0-linux-x86_64.tar.gz
+   
+ cd /home/oliver/k8s/google-cloud-sdk
+    ./install.sh
+    gcloud init
